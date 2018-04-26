602f601076d3ce5432d153e3bd8e871d8401c91b
prateek951 <prince.cenation@gmail.com>
2018-04-27 00:24:27 +0530

    added the bcrypt-nodejs for hashing the plain text and comparing passwords

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 const userSchema = mongoose.Schema({
 
     email : {
@@ -22,7 +24,43 @@ const userSchema = mongoose.Schema({
             ref: 'Tweet'
         }
     ]
-
 });
 
-module.exports = User = mongoose.model('User',userSchema);
+/*Before saving the user to the database we need to hash the password*/
+userSchema.pre('save',function(next){
+    var user = this;
+    if(!user.isModified('password')) 
+        return next();
+    if(user.password){
+        
+        /*If there is a password simply hash the password*/
+        bcrypt.genSalt(10,function(err,salt){
+            bcrypt.hash(user.password,salt,null,function(err,hash){
+                if(err){
+                    return next(err);
+                }
+                /*Set the user password to the hash*/ 
+                user.password = hash;
+                next(err);
+            });
+        }); 
+    }
+});
+
+/*Method for the user gravatar*/
+userSchema.methods.gravatar = function(size){
+        /*If size is not specified specify the size*/
+        if(!size) size = 200;
+        if(!this.email)
+            return 'https://gravatar.com/avatar/?s='+size + '&d=retro'; 
+
+        var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+        return 'https://gravatar.com/avatar/?s=' + md5 + '?s=' + size + '&d=retro';          
+}; 
+
+/*Compare passwords*/ 
+userSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password,this.password);
+}
+
+module.exports = mongoose.model('User',userSchema);
--- routes/main.js
+++ routes/main.js
@@ -13,12 +13,15 @@ router.get('/',(req, res) => res.render('main/landing'));
 /*@desc Create a new user/ 
 /*@access Public*/
 
+
 router.post('/new',(req, res) => {
+    
+    const user = new User();
+    user.email = req.body.username;
+    user.password = req.body.password;
+    user.name = req.body.name;
+    user.save().then(user => res.json('user saved successfully to the database'))
+    .catch(err => res.status(500).json('an error occured while trying to save the user to the database'));
 
-    /*Tap the values of input fields pertaining to the user*/
-    new User(req.body).save().then(user => res.json(user))
-    .catch(err => res.status(500).json(err));
-});
-
-
+})
 module.exports = router;
