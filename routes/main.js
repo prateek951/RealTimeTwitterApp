5f28aa211ce0d24aea51a2c78ad498f933012aa0
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-04-27 02:16:53 +0530

    ..

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const User = require('../models/user');
 const router = express.Router();
 
