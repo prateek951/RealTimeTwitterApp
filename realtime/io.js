da038e2ad6a8f7869e42a06dfa15b1a706a97650
prateek951 <prince.cenation@gmail.com>
2018-05-06 19:25:24 +0530

    refactored the socketio comm

--- realtime/io.js
+++ realtime/io.js
@@ -1,42 +1,47 @@
-const async = require('async');
-const Tweet = require('../models/tweet');
-const User = require('../models/user');
-module.exports = function(io){
+const async = require("async");
+const Tweet = require("../models/tweet");
+const User = require("../models/user");
 
-    io.on('connection',function(socket){
-        console.log('Connected');
-        var user = socket.request.user;
-        console.log(user.name);
+module.exports = function(io) {
+  io.on("connection", function(socket) {
+    console.log("Connected");
+    var user = socket.request.user;
+    console.log(user.name);
 
-        socket.on('tweet',data => {
-            async.parallel([
-                function(callback){
-                    io.emit('incomingTweets',{data,user});
+    socket.on("tweet", data => {
+      console.log(data);
+      async.parallel([
+        function(callback) {
+          io.emit("incomingTweets", { data, user });
+        },
+
+        function(callback) {
+          async.waterfall([
+            function(callback) {
+              var tweet = new Tweet();
+              tweet.content = data.content;
+              tweet.owner = user._id;
+              tweet.save(function(err) {
+                callback(err, tweet);
+              });
+            },
+
+            function(tweet, callback) {
+              User.update(
+                {
+                  _id: user._id
                 },
-                function(callback){
-                    async.waterfall([
-                        function(callback){
-                            var tweet = new Tweet();
-                            tweet.content = data.content;
-                            tweet.owner = user._id;
-                            tweet.save(err => callback(err,tweet));       
-                        }
-                    ]),
-                    function(tweet,callback){
-                        User.update({
-                            _id : user._id
-                        },
-                       { $push: {
-                            tweets: {tweet: tweet._id}
-                        }},function(err,count){
-                            callback(err,count);
-                        }
-                    );
-                    }
-
+                {
+                  $push: { tweets: { tweet: tweet._id } }
+                },
+                function(err, count) {
+                  callback(err, count); // end of the code
                 }
-            ]);
-        })
+              );
+            }
+          ]);
+        }
+      ]);
     });
-}
-
+  });
+};
